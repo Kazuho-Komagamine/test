baffc7ef5e55c923926a932bd12ba8d199c8e5b8
Kazuho-Komagamine <4KaNEwork@gmail.com>
2018-06-20 17:06:26 +0900

    6/20

---
+++ Java演習/オブジェクト指向/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld{
+    public static void main(String[]args){
+        System.out.println("Hello World");
+
+        String s = new String();
+        s = "Hello";
+        System.out.println(s);
+
+        String t = "This is a pen";
+        System.out.println(t);
+
+    }
+}
